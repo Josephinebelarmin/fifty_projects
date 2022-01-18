bffc034b7b5db8f2e29a4af86140d78555952160
Josephine <88236954+Josephinebelarmin@users.noreply.github.com>
2022-01-18 18:25:16 -0500

    Add files via upload

--- 18-background-slider/script.js
+++ 18-background-slider/script.js
@@ -3,11 +3,40 @@ const slides = document.querySelectorAll('.slide')
 const leftBtn = document.getElementById('left')
 const rightBtn = document.getElementById('right')
 
-let activeslides = 0;
+let activeSlide = 0
+
+rightBtn.addEventListener('click',() =>{
+    activeSlide++
+
+    if(activeSlide > slides.length -1)
+    {
+        activeSlide = 0
+    }
+    setBgToBody()
+    setActiveSlide()
+})
+
+leftBtn.addEventListener('click',() =>{
+    activeSlide--
+
+    if(activeSlide < 0)
+    {
+        activeSlide = slides.length -1
+    }
+    setBgToBody()
+    setActiveSlide()
+})
+
+
+
 function setBgToBody(){
-    body.style.backgroundImage = slides[activeslide].style.backgroundImage
+    body.style.backgroundImage = slides[activeSlide].style.backgroundImage
 }
 
-function setActiveslide{
-    
+function setActiveSlide(){
+    slides.forEach(slide=>{
+        slide.classList.remove('active')
+    })
+
+    slides[activeSlide].classList.add('active')
 }
